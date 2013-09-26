5d0f1b5684b459958118c4ef4eb17b25740e688a
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2013-09-26 12:04:47 +0100

    basic kiosk options: non title, ignore back, make launcher

--- src/org/opensharingtoolkit/kiosk/MainActivity.java
+++ src/org/opensharingtoolkit/kiosk/MainActivity.java
@@ -2,11 +2,15 @@ package org.opensharingtoolkit.kiosk;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-    @Override
+    private static final String TAG = "kiosk";
+
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -15,9 +19,17 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+    	// actually, there should never be a menu in the kiosk
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onBackPressed() {
+		// no-op
+		Log.w(TAG,"ignoring Back");
+	}    
     
 }
