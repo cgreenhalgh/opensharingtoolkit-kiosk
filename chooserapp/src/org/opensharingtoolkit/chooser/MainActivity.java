b1dedf0065fe08128fdd43da2cd8a8570b34d509
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2014-01-21 15:56:35 +0000

    fix chooserapp orientation and (try to) keep on-screen; more details on kiosking steps

--- chooserapp/src/org/opensharingtoolkit/chooser/MainActivity.java
+++ chooserapp/src/org/opensharingtoolkit/chooser/MainActivity.java
@@ -1,14 +1,21 @@
 package org.opensharingtoolkit.chooser;
 
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.WindowManager;
 
 public class MainActivity extends BrowserActivity {
 
-	
-	
 	@Override
 	@SuppressLint("SetJavaScriptEnabled")
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +23,103 @@ public class MainActivity extends BrowserActivity {
 		startService(new Intent(getApplicationContext(), Service.class));
 		
 		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);		
+		super.onCreate(savedInstanceState);	
+		
+        // keep screen on (if possible) 
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        // This seems to be specific to CyanogenMod:
+        // getWindow().addFlags(WindowManager.LayoutParams.PREVENT_POWER_KEY);
+        
+        // catch screen off
+        // see http://stackoverflow.com/questions/9886466/overriding-the-power-button-in-android
+        mScreenReceiver = new ScreenReceiver();
+        final IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenReceiver, filter);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		unregisterReceiver(mScreenReceiver);
+		finishWakeLocker();
+		super.onDestroy();
+	}
+	
+	private long mStopTime;
+	@Override
+	protected void onPause() {
+		mStopTime = System.currentTimeMillis();
+		super.onPause();
 	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mStopTime = 0;
+		finishWakeLocker();
+	}
+
+
+
+	private WakeLock mWakeLock;
+	
+	class ScreenReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if ((intent.getAction().equals(Intent.ACTION_SCREEN_OFF))) {
+				Log.w(TAG,"ACTION_SCREEN_OFF");
+
+				long now = System.currentTimeMillis();
+				if (mStopTime==0 || now-mStopTime < 1000) {
+					Log.w(TAG,"Try to restart...");
+					PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+					if (mWakeLock==null)
+						mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE, "TEST");
+					mWakeLock.acquire();
+				
+					AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+					Intent inten = new Intent(context,MainActivity.class);
+					PendingIntent pi = PendingIntent.getActivity(context, 0, inten, 0);
+					alarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,  100, pi);
+				}
+			}
+		}   
+	}	
+	// Finish your WakeLock HERE. call this method after U put the activity in front or when u exit from the new activity.
+	private void finishWakeLocker(){
+		if (mWakeLock != null) {
+			mWakeLock.release();
+			mWakeLock = null;
+		}
+	}
+	
+	private ScreenReceiver mScreenReceiver;
+	
 	@Override
 	protected boolean handleBackPressed() {
 		if (!super.handleBackPressed())
 			// always 'handled'?? leave to kiosk??
 			Log.w(TAG,"ignoring Back");
 		return true;
+	}
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    if (keyCode == KeyEvent.KEYCODE_POWER) {
+	        // Do something here...
+	    	Log.w(TAG,"POWER KeyDown ignored");
+	        return true;
+	    }
+	    return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+	    if (keyCode == KeyEvent.KEYCODE_POWER) {
+	        // Do something here...
+	    	Log.w(TAG,"POWER KeyLongPress ignored");
+	        return true;
+	    }
+	    return super.onKeyLongPress(keyCode, event);
 	}
 }
--- kioskapp/README.md
+++ kioskapp/README.md
@@ -2,5 +2,65 @@
 
 Optional application which is responsible for the actual kiosking of the device, i.e. preventing access to other applications or settings from the normal screen. This includes the launcher functionality.
 
-Status: place-holder
+Status: Simple/minimal launcher
+
+## Additional instructions
+
+Kiosking requires a number of additional steps. Much of this is based on (VAUGHNTEEGARDEN's post)[http://thebitplague.wordpress.com/2013/04/05/kiosk-mode-on-the-nexus-7/]
+
+### Setting up
+
+Once the app is installed you should press `home` and then select this app as the launcher `always`.
+
+### Screen lock settings
+
+In settings > Lock screen > Screen security (or equivalent) change the Screen lock to `none`.
+
+You could also change the Display > Sleep setting so that it will normally turn off the screen more slowly, but the chooser application should actually keep the screen on all the time it is running anyway. 
+
+### Boot when powered
+
+To ensure it will boot on power, boot into the bootloader (i.e. fastboot mode), and do
+```
+fastboot oem off-mode-charge 0
+```
+
+### Hide soft keys
+
+This is really important for kiosking functionality - otherwise back, home, processes will still be available, and the last especially will give people access to the system. But this will only be useful if your device does NOT have hardware keys for home, etc.
+
+To hide soft keys, enable USB debugging, give permission to your dev machine, root the device, then get `build.prop` off the device
+```
+adb pull /system/build.prop
+```
+Add the following entry to it:
+```
+# disable soft keys
+qemu.hw.mainkeys=1
+```
+Re-mount the system filesystem read-write - first check its type and path:
+```
+adb shell
+mount
+```
+Look for the `system` entry, e.g. 
+```
+/dev/block/platform/sdhci-tegra.3/by-name/APP /system ext4 ro,relatime,user_xattr,acl,barrier=1,data=ordered 0 0
+```
+Now remount using something like: 
+```
+mount -o remount,rw -t TYPE PATH
+``` 
+where `PATH` is the first path you see and `TYPE` is the filesystem type, e.g. `ext4` i the above example.
+
+Finally you can push back the changed file:
+```
+adb push build.prop /system/build.prop
+```
+Of course, if you have (e.g.) `vi` or another editor installed on the device then you can (with root permission) edit `build.prop` directly after `system` has been remounted `rw`.
+
+Now reboot.
+
+TO go back to normal do essentially the same but comment out or remove the line `qemu.hw.mainkeys=1`. 
+
 
