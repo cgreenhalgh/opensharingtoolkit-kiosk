f3f0867dd67f21e6cf026a9a3bf3239b3f86ecea
cgreenhalgh <chris.greenhalgh@nottingham.ac.uk>
2014-01-22 17:11:50 +0000

    failed attempt to disable pull down of status bar

--- chooserapp/src/org/opensharingtoolkit/chooser/MainActivity.java
+++ chooserapp/src/org/opensharingtoolkit/chooser/MainActivity.java
@@ -1,5 +1,8 @@
 package org.opensharingtoolkit.chooser;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -7,11 +10,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.WindowManager;
 
 import org.opensharingtoolkit.common.Recorder;
@@ -57,16 +63,106 @@ public class MainActivity extends BrowserActivity {
 		mRecorder.i("activity.pause", null);
 		mStopTime = System.currentTimeMillis();
 		super.onPause();
+	    // Activity's been paused      
+	    //isPaused = true;
 	}
 
+    
+    // http://stackoverflow.com/questions/7821584/error-using-statusbarmanagerservice-java-lang-securityexception-on-android-per
+	// tried (below) but doesn't seem to help at all on 4.4.2
+	// See also http://e2e.ti.com/support/embedded/android/f/509/t/283260.aspx
+    //   mStatusBarManager = (StatusBarManager) appGetSystemService(Context.STATUS_BAR_SERVICE);
+    //   mStatusBarManager.disable(StatusBarManager.DISABLE_EXPAND)
+    // Hmm, but see
+	//   http://stackoverflow.com/questions/19920052/disable-the-notification-panel-from-being-pulled-down
+	// Probably need a custom status bar or something like that installed as part of the OS
+	/*
+	// To keep track of activity's foreground/background status
+	private boolean isPaused;
+	// To keep track of activity's window focus
+	private boolean currentFocus;
+
+
+	private Handler collapseNotificationHandler;
+	
+	@Override
+	// See http://stackoverflow.com/questions/19920052/disable-the-notification-panel-from-being-pulled-down
+	public void onWindowFocusChanged(boolean hasFocus) {
+	    currentFocus = hasFocus;
+	    if (!hasFocus) {
+	        // Method that handles loss of window focus
+	        collapseNow();
+	    }
+	}
+	// See http://stackoverflow.com/questions/19920052/disable-the-notification-panel-from-being-pulled-down
+	public void collapseNow() {
+	    // Initialize 'collapseNotificationHandler'
+	    if (collapseNotificationHandler == null) {
+	        collapseNotificationHandler = new Handler();
+	    }
+	    // If window focus has been lost && activity is not in a paused state
+	    // Its a valid check because showing of notification panel
+	    // steals the focus from current activity's window, but does not 
+	    // 'pause' the activity
+	    if (!currentFocus && !isPaused) {
+	        // Post a Runnable with some delay - currently set to 300 ms
+	        collapseNotificationHandler.postDelayed(new Runnable() {
+	            @Override
+	            public void run() {
+	                // Use reflection to trigger a method from 'StatusBarManager'                
+	                Object statusBarService = getSystemService("statusbar");
+	                Class<?> statusBarManager = null;
+	                try {
+	                    statusBarManager = Class.forName("android.app.StatusBarManager");
+		                Method collapseStatusBar = null;
+		                try {
+		                    // Prior to API 17, the method to call is 'collapse()'
+		                    // API 17 onwards, the method to call is `collapsePanels()`
+		                    if (Build.VERSION.SDK_INT > 16) {
+		                        collapseStatusBar = statusBarManager.getMethod("collapsePanels");
+		                    } else {
+		                        collapseStatusBar = statusBarManager.getMethod("collapse");
+		                    }
+		                    collapseStatusBar.setAccessible(true);
+
+			                try {
+			                    collapseStatusBar.invoke(statusBarService);
+			                    Log.d(TAG,"Requested collapse status bar");
+				                // Check if the window focus has been returned
+				                // If it hasn't been returned, post this Runnable again
+				                // Currently, the delay is 100 ms. You can change this
+				                // value to suit your needs.
+				                if (!currentFocus && !isPaused) {
+				                    collapseNotificationHandler.postDelayed(this, 100L);
+				                }
+			                } catch (Exception e) {
+			                	Log.w(TAG,"Error inoking collapse method", e);
+			                }
+		                } catch (NoSuchMethodException e) {
+		                    Log.e(TAG,"Error getting collapsePanels/collapse", e);
+		                }
+	                } catch (ClassNotFoundException e) {
+	                    Log.e(TAG,"Error getting StatusBarManager", e);
+	                }
+	            }
+	        }, 300L);
+	    }   
+	}
+	*/
 	@Override
 	protected void onResume() {
 		mRecorder.i("activity.resume", null);
 		super.onResume();
+		// hide notification??
+		View decorView = getWindow().getDecorView();
+		int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+		decorView.setSystemUiVisibility(uiOptions);
+
 		mStopTime = 0;
 		finishWakeLocker();
+	    // Activity's been resumed
+	    //isPaused = false;
 	}
-
 
 
 	private WakeLock mWakeLock;
