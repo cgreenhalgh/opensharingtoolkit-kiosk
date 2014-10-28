cgreenhalgh/opensharingtoolkit-kiosk

commit 924d46740619dc8feaea52104a1fb79ef56b79d6

fix #10 kiosk entry icon low quality; fix #9 kiosk preview and send local from cache fail for paths with %-escapes

chooserapp/assets/application.js
@@ -648,7 +648,7 @@
         console.log("Removed explicit port from kiosk path " + path + " -> " + newpath);
       }
     }
-    url = nocache ? enc.url : (_ref = enc.path) != null ? _ref : enc.url;
+    url = nocache ? enc.url : (_ref = encodeURI(enc.path)) != null ? _ref : enc.url;
     url = kiosk.getPortableUrl(url);
     console.log("get " + entry.attributes.title + " as " + url + ", enc " + enc.path + "  / " + enc.url);
     baseurl = nocache && (entry.attributes.baseurl != null) ? entry.attributes.baseurl : window.location.href;
@@ -3399,7 +3399,7 @@
       console.log("option:view entry " + this.model.id);
       if (!kiosk.getSafePreview() && !this.model.attributes.isKiosk) {
         enc = this.model.attributes.enclosures[0];
-        url = !kiosk.isKiosk() ? enc.url : (_ref = enc.path) != null ? _ref : enc.url;
+        url = !kiosk.isKiosk() ? enc.url : (_ref = encodeURI(enc.path)) != null ? _ref : enc.url;
         url = kiosk.getPortableUrl(url);
         console.log("view " + this.model.attributes.title + " as " + url + ", enc " + enc.path + "  / " + enc.url);
         if (!kiosk.openUrl(url, enc.mime)) {

chooserjs/public/application.js
@@ -648,7 +648,7 @@
         console.log("Removed explicit port from kiosk path " + path + " -> " + newpath);
       }
     }
-    url = nocache ? enc.url : (_ref = enc.path) != null ? _ref : enc.url;
+    url = nocache ? enc.url : (_ref = encodeURI(enc.path)) != null ? _ref : enc.url;
     url = kiosk.getPortableUrl(url);
     console.log("get " + entry.attributes.title + " as " + url + ", enc " + enc.path + "  / " + enc.url);
     baseurl = nocache && (entry.attributes.baseurl != null) ? entry.attributes.baseurl : window.location.href;
@@ -3399,7 +3399,7 @@
       console.log("option:view entry " + this.model.id);
       if (!kiosk.getSafePreview() && !this.model.attributes.isKiosk) {
         enc = this.model.attributes.enclosures[0];
-        url = !kiosk.isKiosk() ? enc.url : (_ref = enc.path) != null ? _ref : enc.url;
+        url = !kiosk.isKiosk() ? enc.url : (_ref = encodeURI(enc.path)) != null ? _ref : enc.url;
         url = kiosk.getPortableUrl(url);
         console.log("view " + this.model.attributes.title + " as " + url + ", enc " + enc.path + "  / " + enc.url);
         if (!kiosk.openUrl(url, enc.mime)) {

chooserjs/app/getter.coffee
@@ -21,7 +21,7 @@ module.exports.getGetUrl = (entry, devicetype, nocache) ->
       console.log "Removed explicit port from kiosk path #{path} -> #{newpath}"
 
   # use cache copy if available
-  url = if nocache then enc.url else (enc.path ? enc.url)
+  url = if nocache then enc.url else (encodeURI( enc.path ) ? enc.url)
   url = kiosk.getPortableUrl url
   console.log "get #{entry.attributes.title} as #{url}, enc #{enc.path}  / #{enc.url}"
 

chooserjs/app/views/EntryInfo.coffee
@@ -82,7 +82,7 @@ module.exports = class EntryInfoView extends Backbone.View
     console.log "option:view entry #{ @model.id }"
     if not kiosk.getSafePreview() and not @model.attributes.isKiosk
       enc = @model.attributes.enclosures[0]
-      url =  if not kiosk.isKiosk() then enc.url else (enc.path ? enc.url)
+      url =  if not kiosk.isKiosk() then enc.url else (encodeURI( enc.path ) ? enc.url)
       url = kiosk.getPortableUrl url
       console.log "view #{@model.attributes.title} as #{url}, enc #{enc.path}  / #{enc.url}"
       if not kiosk.openUrl url, enc.mime

commit 5a04ac5a300a792317229321347daa39e5aa0081

fix local file from cache url escaping (regression caused by commit 924d4674)

chooserapp/assets/application.js
@@ -1125,8 +1125,8 @@
         return prefix + file.path;
       } else {
         if (isRelativeUrl(url)) {
-          console.log("Relative url " + url + " assumed cached");
-          return prefix + url;
+          console.log("Relative url " + url + " assumed cached as " + (decodeURI(url)));
+          return prefix + decodeURI(url);
         } else {
           return null;
         }

chooserjs/app/loader.coffee
@@ -35,8 +35,8 @@ getCachePath = (url,cacheFiles,prefix) ->
     else 
       if isRelativeUrl url
         # relative url
-        console.log "Relative url #{url} assumed cached"
-        prefix+url
+        console.log "Relative url #{url} assumed cached as #{decodeURI url}"
+        prefix+decodeURI( url )
       else
         null
   else

chooserjs/public/application.js
@@ -1125,8 +1125,8 @@
         return prefix + file.path;
       } else {
         if (isRelativeUrl(url)) {
-          console.log("Relative url " + url + " assumed cached");
-          return prefix + url;
+          console.log("Relative url " + url + " assumed cached as " + (decodeURI(url)));
+          return prefix + decodeURI(url);
         } else {
           return null;
         }